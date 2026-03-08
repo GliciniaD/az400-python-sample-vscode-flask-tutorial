d8ed07a67c3fd4da7c356239baee7d234c0752eb
GliciniaD <190259001+GliciniaD@users.noreply.github.com>
2026-03-08 12:17:14 +0000

    test to see if all pipelines run again

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask!If you can see this, the code has been deployed to your webapp"
+    return "Hello from Flask!If you can see this, the code has been deployed to your webapp. Test to see if all pipelines run again"
